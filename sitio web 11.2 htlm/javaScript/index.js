c597c6cc79326ae6185be92d5e75214e99ff9b9a
Jesus-Alberto878 <jesusalbertocortes24@cbtis52.edu.mx>
2026-05-05 16:21:36 -0600

    Add files via upload
    Se agrego más texto a todas las paginas que no estaban como validas para el codecheck de HTML y CSS, ahora solo la que no detecta como valida es reporte .html

---
+++ sitio web 11.2 htlm/javaScript/index.js
@@ -0,0 +1,25 @@
+const botonMenu = document.getElementById("menu");
+const menuLateral = document.querySelector(".menuLateral");
+
+// abrir/cerrar con el botón
+botonMenu.addEventListener("click", (e) => {
+    e.stopPropagation(); // evita que el click se propague al document
+    menuLateral.classList.toggle("active");
+});
+
+// evitar que al hacer click dentro del menú se cierre
+menuLateral.addEventListener("click", (e) => {
+    e.stopPropagation();
+});
+
+// cerrar al hacer click fuera
+document.addEventListener("click", () => {
+    menuLateral.classList.remove("active");
+});
+
+const botonEmpezar = document.getElementById("btnEmpezar");
+
+botonEmpezar.addEventListener("click",(e) => {
+    e.stopPropagation();
+    window.location.href = "pagina2.html"
+} )
